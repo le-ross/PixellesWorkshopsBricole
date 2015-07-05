0b723eaaf12901848ad66d407b35b37bcd1c787e
Ross Moussouni <ross.moussouni@gmail.com>
2015-07-05 13:03:12 -0400

    added some scenes

---
+++ Assets/ChangeColorOnDirection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorOnDirection : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var light = this.GetComponent<Light> ();
+
+
+		var redChannel = Mathf.Abs (this.transform.position.x);
+		var greenChannel = Mathf.Abs (this.transform.position.y - 2f);
+		var blueChannel = Mathf.Abs (this.transform.position.z);
+		light.color = new Color (redChannel, greenChannel, blueChannel);
+
+	}
+}

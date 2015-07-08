62ffd68fd39d1b49ded76d95c2f04db381480b0f
Ross Moussouni <ross.moussouni@gmail.com>
2015-07-08 15:56:35 -0400

    that the sound of the police

---
+++ Assets/FlashingLights.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingLights : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		StartCoroutine (FlashThatLight ());
+	}
+	
+	IEnumerator FlashThatLight ()
+	{
+		var light = this.GetComponent<Light> ();
+		var minLight = 1f;
+		var maxLight = 8f;
+
+		while (true) {
+
+			while (light.intensity > minLight) {
+				light.intensity -= 0.5f;
+				yield return new WaitForSeconds (0.01f);
+			}
+
+			while (light.intensity < maxLight) {
+				light.intensity += 0.5f;
+				yield return new WaitForSeconds (0.01f);
+			}
+		}
+	}
+}
